fb85373c9798afb68eafc34e2f140de5ef303693
Johannes Rauh <johannes.rauh@icinga.com>
2024-06-21 12:43:05 +0200

    Start and stop tests on tester

--- cmd/tester/main.go
+++ cmd/tester/main.go
@@ -8,11 +8,13 @@ import (
 	"github.com/icinga/icinga-go-library/types"
 	schemav1 "github.com/icinga/icinga-kubernetes/pkg/schema/v1"
 	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	kclientcmd "k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 	"os"
+	"runtime"
 	"time"
 )
 
@@ -38,6 +40,60 @@ func getPodUuid(clientset *kubernetes.Clientset, podName string, podNamespace st
 	}
 
 	return schemav1.EnsureUUID(pod.UID), nil
+}
+
+func startCpuTest(ctx context.Context) error {
+	klog.Info("Starting cpu test")
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	numCPU := runtime.NumCPU()
+	for i := 0; i < numCPU; i++ {
+		g.Go(func() error {
+			for {
+				//_ = math.Sin(math.Pi)
+				klog.Info("CPU TEST")
+
+				select {
+				case <-ctx.Done():
+					klog.Info("Stopping cpu test")
+					return ctx.Err()
+				case <-time.After(1 * time.Second):
+					//case <-time.After(200 * time.Nanosecond):
+				}
+			}
+		})
+	}
+
+	return g.Wait()
+}
+
+func startMemoryTest(ctx context.Context) error {
+	klog.Info("Starting memory test")
+
+	//mem := make([]byte, 0)
+
+	for {
+		//mem = append(mem, make([]byte, 200*1024*1024)...) // Allocate 100MB
+		klog.Info("MEMORY TEST")
+
+		select {
+		case <-ctx.Done():
+			klog.Info("Stopping memory test")
+			return ctx.Err()
+		case <-time.After(1 * time.Second):
+			//case <-time.After(500 * time.Millisecond):
+		}
+	}
+}
+
+func contains(slice []string, item string) bool {
+	for _, a := range slice {
+		if a == item {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -74,24 +130,98 @@ func main() {
 		klog.Fatal(errors.Wrap(err, "Can't get pod UUID"))
 	}
 
-	for {
-		rows, err := db.Query("SELECT test FROM pod_test WHERE pod_uuid = ?", uuid)
-		if err != nil {
-			klog.Fatal(errors.Wrap(err, fmt.Sprintf("Can't get tests for pod %s", podName)))
-		}
+	g, ctx := errgroup.WithContext(context.Background())
+	testStopChans := make(map[string]context.CancelFunc)
 
-		var test string
-		var tests []string
+	g.Go(func() error {
+		var currentTests []string
 
-		for rows.Next() {
-			err = rows.Scan(&test)
+		for {
+			rows, err := db.Query("SELECT test FROM pod_test WHERE pod_uuid = ?", uuid)
 			if err != nil {
-				klog.Fatal(errors.Wrap(err, "Can't scan row"))
+				klog.Fatal(errors.Wrap(err, fmt.Sprintf("Can't get tests for pod %s", podName)))
 			}
-			klog.Info(fmt.Sprintf("%s: %s", uuid.String(), test))
-			tests = append(tests, test)
+
+			var test string
+			var newTests []string
+
+			for rows.Next() {
+				err = rows.Scan(&test)
+				if err != nil {
+					klog.Fatal(errors.Wrap(err, "Can't scan row"))
+				}
+
+				newTests = append(newTests, test)
+			}
+
+			for _, currentTest := range currentTests {
+				if !contains(newTests, currentTest) {
+					if stopFunc, ok := testStopChans[currentTest]; ok {
+						stopFunc()
+						delete(testStopChans, currentTest)
+					}
+				}
+			}
+
+			for _, newTest := range newTests {
+				if !contains(currentTests, newTest) {
+					testCtx, cancel := context.WithCancel(context.Background())
+					testStopChans[newTest] = cancel
+					switch newTest {
+					case "cpu":
+						g.Go(func() error {
+							if err := startCpuTest(testCtx); err != nil {
+								klog.Error(errors.Wrap(err, fmt.Sprintf("Error running test %s", newTest)))
+								return err
+							}
+
+							return nil
+						})
+					case "memory":
+						g.Go(func() error {
+							if err := startMemoryTest(testCtx); err != nil {
+								klog.Error(errors.Wrap(err, fmt.Sprintf("Error running test %s", newTest)))
+								return err
+							}
+
+							return nil
+						})
+					}
+				}
+			}
+
+			currentTests = newTests
+
+			time.Sleep(5 * time.Second)
+		}
+	})
+
+	g.Go(func() error {
+		<-ctx.Done()
+
+		for _, cancel := range testStopChans {
+			cancel()
 		}
 
-		time.Sleep(5 * time.Second)
+		return nil
+	})
+
+	//g.Go(func() error {
+	//	for {
+	//		for _, test := range activeTests {
+	//			switch test {
+	//			case "cpu":
+	//				if err := startCpuTest(ctx); err != nil {
+	//					return err
+	//				}
+	//			}
+	//		}
+	//	}
+	//})
+
+	if err := g.Wait(); err != nil {
+		klog.Fatal(err)
 	}
+
+	klog.Info("Exiting")
 }
